663a96cc426bf245ae04cadd7ab86821be5339a9
Aderevs <aderevs@gmail.com>
2024-03-29 17:36:28 +0200

    task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+namespace Task3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            var app = builder.Build();
+
+            app.MapControllerRoute(
+               name: "default",
+               pattern: "{controller}/{action}");
+            app.Run();
+        }
+    }
+}
